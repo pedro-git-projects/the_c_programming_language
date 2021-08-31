7ab85cb85d39084a941965f137c4cdfd036edb2f
pedro-git-projects <pedro.coding.contact@gmail.com>
2021-08-31 00:59:18 -0300

    started chapter 1

---
+++ examples/001_hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h> //include information about standard library
+
+//define a function named main that recieves no argument values
+main() 
+{ //statements of main are enclosed in braces
+	printf("Hello, world\n"); //main calls library function printf to print a sequence of characters;
+	// \n represents the newline character
+}
